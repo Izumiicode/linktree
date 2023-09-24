fb435ee1e3854518d3ba915b1c79028c4fc02c04
Izumii <123919078+Izumiicode@users.noreply.github.com>
2023-09-24 13:35:53 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const actionLink = document.querySelectorAll(".link-card .link-action");
+
+actionLink.forEach((action) => {
+  action.addEventListener("click", (e) => {
+    e.preventDefault();
+    navigator.clipboard.writeText(action.parentElement.getAttribute("href"));
